fa4cd0a3abf8d4f74561530b2fbe46edd9e49ef9
Arijit Mukherjee <armuk@microsoft.com>
2020-04-16 22:52:03 +0530

    testing fb login

--- webApp/app.py
+++ webApp/app.py
@@ -31,7 +31,7 @@ def put_im():
 @app.route('/get_im')
 def get_im():
     
-    return '<img src="'+str(img[-1])+'"/>'+str(img[-1])
+    return '<img src="'+str(img[-1])+'"/>'
 
 @app.route('/')
 def home():
